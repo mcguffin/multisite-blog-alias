9db9492f897af5cf5332e51356893d431a81461f
mcguffin <joern@flyingletters.com>
2023-09-30 08:46:08 +0200

    cleanup core class

--- include/BlogAlias/Core/Core.php
+++ include/BlogAlias/Core/Core.php
@@ -14,39 +14,4 @@ use BlogAlias\Asset;
 
 class Core extends Plugin implements CoreInterface {
 
-	/**
-	 *	@inheritdoc
-	 */
-	protected function __construct() {
-
-		add_action( 'init' , array( $this , 'init' ) );
-
-		add_action( 'wp_enqueue_scripts' , array( $this , 'enqueue_assets' ) );
-
-		$args = func_get_args();
-		parent::__construct( ...$args );
-	}
-
-	/**
-	 *	Load frontend styles and scripts
-	 *
-	 *	@action wp_enqueue_scripts
-	 */
-	public function enqueue_assets() {
-	}
-
-
-
-
-
-
-	/**
-	 *	Init hook.
-	 *
-	 *  @action init
-	 */
-	public function init() {
-	}
-
-
 }
